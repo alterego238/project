89aa3671923e8169aacb071058e94cf9ab5331d3
alterego238 <2949764281@qq.com>
2024-11-08 14:51:18 +0800

    bug

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -9,8 +9,8 @@ class MainTest {
 	@Test
 	void test() {
 		Main main = new Main();
-		int result = main.add(1, 1);
-		assertEquals(2, result);
+		int result = main.add(-1, -1);
+		assertEquals(-2, result);
 	}
 
 }
